0a41434e8d5d825d870f98ce161b3cc5e442bd74
kevinpagtakhan <kevpagtakhan@gmail.com>
2016-11-11 18:35:15 -0800

    EditableTimer

--- public/app.js
+++ public/app.js
@@ -23,16 +23,38 @@ const EditableTimerList = React.class({
           project='Project 1'
           elapsed='100'
           runningSince={null}
-          editForm={false}
+          editFormOpen={false}
         />
         <EditableTimer
           title='Title 2'
           project='Project 1'
           elapsed='50'
           runningSince={null}
-          editForm={true}
+          editFormOpen={true}
         />
       </div>
     )
   }
 });
+
+const EditableTimer = React.create({
+  render: function(){
+    if (this.props.editFormOpen){
+      return (
+        <TimerForm
+          title={this.props.title}
+          project={this.props.project}
+        />
+      )
+    } else {
+      return (
+        <Timer
+          title={this.props.title}
+          project={this.props.project}
+          elapsed={this.props.elapsed}
+          runningSince={this.props.runningSince}
+        />
+      )
+    }
+  }
+})
