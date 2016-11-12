5dfb7c5dd67122527f5e7d815a0721049964d495
kevinpagtakhan <kevpagtakhan@gmail.com>
2016-11-11 18:42:03 -0800

    TimerForm

--- public/app.js
+++ public/app.js
@@ -57,4 +57,28 @@ const EditableTimer = React.class({
       )
     }
   }
-})
+});
+
+const TimerForm = React.class({
+  render: function(){
+    const submitText = this.props.title ? 'Update' : 'Create';
+    return (
+      <div className='ui centered card'>
+        <div className='content'>
+          <div className='field'>
+            <label>Title</label>
+            <input type='text' defaultValue={this.props.title} />
+          </div>
+          <div className='field'>
+            <label>Project</label>
+            <input type='text' defaultValue={this.props.project} />
+          </div>
+          <div className='ui two bottom attached buttons'>
+            <button className='ui basic blue button'>{submitText}</button>
+            <button className='ui basic red button'>Cancel</button>
+          </div>
+        </div>
+      </div>
+    )
+  }
+});
