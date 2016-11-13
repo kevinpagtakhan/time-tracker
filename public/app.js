14434652aab2888e71e95863c80d971a715a9bae
kevinpagtakhan <kevpagtakhan@gmail.com>
2016-11-13 15:34:37 -0800

    Delete timer

--- public/app.js
+++ public/app.js
@@ -26,6 +26,9 @@ const TimersDashboard = React.createClass({
   handleEditFormSubmit: function(attrs){
     this.updateTimer(attrs);
   },
+  handleTrashClick: function(timerId){
+    this.deleteTimer(timerId);
+  },
   createTimer: function(timer){
     const t = helpers.newTimer(timer);
     this.setState({
@@ -46,6 +49,13 @@ const TimersDashboard = React.createClass({
       })
     })
   },
+  deleteTimer: function(timerId){
+    this.setState({
+      timers: this.state.timers.filter((timer) => {
+        return timer.id != timerId
+      })
+    })
+  },
   render: function(){
     return (
       <div className='ui three column centered grid'>
@@ -53,6 +63,7 @@ const TimersDashboard = React.createClass({
           <EditableTimerList
             timers={this.state.timers}
             onFormSubmit={this.handleEditFormSubmit}
+            onTrashClick={this.handleTrashClick}
           />
           <ToggleableTimerForm
             onFormSubmit={this.handleCreateFormSubmit}
@@ -75,6 +86,7 @@ const EditableTimerList = React.createClass({
           elapsed={timer.elapsed}
           runningSince={timer.runningSince}
           onFormSubmit={this.props.onFormSubmit}
+          onTrashClick={this.props.onTrashClick}
         />
       )
     });
@@ -133,6 +145,7 @@ const EditableTimer = React.createClass({
           elapsed={this.props.elapsed}
           runningSince={this.props.runningSince}
           onEditClick={this.handleEditClick}
+          onTrashClick={this.props.onTrashClick}
         />
       )
     }
@@ -223,6 +236,9 @@ const ToggleableTimerForm = React.createClass({
 });
 
 const Timer = React.createClass({
+  handleTrashClick: function(){
+    this.props.onTrashClick(this.props.id)
+  },
   render: function(){
     const elapsedString = helpers.renderElapsedString(this.props.elapsed);
     return (
@@ -244,7 +260,10 @@ const Timer = React.createClass({
             >
               <i className='edit icon'></i>
             </span>
-            <span className='right floated trash icon'>
+            <span
+              className='right floated trash icon'
+              onClick={this.handleTrashClick}
+            >
               <i className='trash icon'></i>
             </span>
           </div>
